84ec9a45a4c33fff31ae1a7fa54f09570db9ab6c
Alex K <bnax.gm@gmail.com>
2021-09-10 01:40:18 +0300

    fixed mistake in scadTranslate

--- scadTranslate.m
+++ scadTranslate.m
@@ -1,14 +1,16 @@
 function translate_result = scadTranslate(varargin)
 %scadTranslate Summary of this function goes here
 %   Detailed explanation goes here
-if max(size(varargin)) == 3
+if max(size(varargin)) == 4
     x = num2str_2(varargin{1});
     y = num2str_2(varargin{2});
     z = num2str_2(varargin{3});
-elseif max(size(varargin)) == 1
+    varargin(1:3) = [];
+elseif max(size(varargin)) == 2
     x = num2str_2(varargin{1}(1));
     y = num2str_2(varargin{1}(2));
     z = num2str_2(varargin{1}(3));
+    varargin(1) = [];
 else
 end
 translate_result = ['translate([' x ','  y ','  z ']){' newline];
