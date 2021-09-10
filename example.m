76caaa550e66034ca7a7a0c6cb8dfa6211c61645
Alex K <bnax.gm@gmail.com>
2021-09-11 01:04:31 +0300

    added scadSphere

--- example.m
+++ example.m
@@ -3,9 +3,9 @@ file_name = 'example3';
 rect_1 = scadRect([10,10,10]);
 rect_2 = scadRect([5.1, 5.1, 5.1], 'color', 'red', 'position', [5, 5, 5]);
 structure = scadTranslate([5,5,5], rect_2);
-structure = scadRotate(structure, 45, [5, 5, 5]) % rotate around axis [5,5,5] for 45deg
-structure = scadHull(char(structure), char(rect_1));
-structure = scadRotate(structure, [45, -45, 45]) ;
+structure = scadRotate(structure, 45, [5, 5, 5]); % rotate around axis [5,5,5] for 45deg
+structure = scadHull(char(structure), char(rect_1)); 
+structure = scadRotate(structure, [45, -45, 45]) ; % rotate around central point [x, y, z]
 
 disp(structure)
 SaveSCAD(file_name, structure);
---
+++ scadSphere.m
@@ -0,0 +1,57 @@
+function sphere = scadSphere(diam, varargin)
+
+%Parameters
+% r Radius. This is the radius of the sphere. The resolution of the sphere
+% is based on the size of the sphere and the $fa, $fs and $fn variables.
+% For more information on these special variables look at:
+% OpenSCAD_User_Manual/Other_Language_Features
+% d
+% Diameter. This is the diameter of the sphere.
+% $fa
+% Fragment angle in degrees
+% $fs
+% Fragment size in mm
+% $fn
+% Resolution
+position = [];
+color = [];
+size = 'd';
+param = '';
+while ~isempty(varargin)
+    switch lower(varargin{1})
+        case '$fa'
+            param = [param ' $fa = ' num2str(varargin{2}) ','];
+            varargin(1:2) = [];
+        case '$fs'
+            param = [param ' $fs = ' num2str(varargin{2}) ',']   ;
+        case '$fn'
+            param = [param ' $fn = ' num2str(varargin{2}) ',']  ;
+        case 'position'
+            position = varargin{2};
+            varargin(1:2) = [];
+        case 'color'
+            color = varargin{2};
+            varargin(1:2) = [];
+        case 'r'
+            size = 'r';
+            varargin(1) = [];
+        case 'd'
+            size = 'd';
+            varargin(1) = [];
+        otherwise
+    end
+end
+if ~isempty(param)
+    param = [', ' param];
+    param(end) = [];
+end
+sphere = (['sphere(' size '=' num2str(diam) param ')' ';' ]);
+if ~isempty(color)
+    sphere = scadColor(color, sphere);
+end
+if ~isempty(position)
+    sphere =  scadTranslate(position, sphere);
+end
+
+end
+
