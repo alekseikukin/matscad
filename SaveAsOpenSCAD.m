f5bc1e089e51d0d658465a48d34b3ea5e7de1860
Alex K <bnax.gm@gmail.com>
2021-09-10 14:41:42 +0300

    added conctrole of file extention in function SaveAsOpenSCAD

--- SaveAsOpenSCAD.m
+++ SaveAsOpenSCAD.m
@@ -1,28 +1,27 @@
 function status = SaveAsOpenSCAD(scad_file_name, varargin)
-%STARTOPENSCAD Summary of this function goes here
+%SaveAsOpenSCAD Summary of this function goes here
 %   Detailed explanation goes here
 output_file_extention = 'stl';
+scad_file_name = FileExtController(scad_file_name, 'scad');
+output_filename = scad_file_name;
 extra_params = {};
 while ~isempty(varargin)
     switch lower(varargin{1})
         case 'scadfolder'
             extra_params{end+1} = 'scadfolder';
-            extra_params{end+1} = varargin{2};
+            extra_params{end+1} = FileExtController(varargin{2}, "/");
         case {'ofe','output_file_extention'}
             output_file_extention = varargin{2};
         otherwise
     end
     varargin(1:2) = [];
 end
-
-
-
-
+output_filename = FileExtController(output_filename, output_file_extention);
 quality = 'production';
 cmd = ['-o ' ...
-    output_filename '.' output_file_extention ...
+     output_filename ...
     ' -D ' '"quality="" ' quality '"""'...
     scad_file_name ];
-cmdOpenSCAD(cmd, extra_params{:});
+status = cmdOpenSCAD(cmd, extra_params{:});
 end
 
