b12f480f3bd90481c7e73c635a1ddd1a38a6879e
Alex K <bnax.gm@gmail.com>
2021-09-16 00:03:47 +0300

    added scadLinearExtrude

--- example3.scad
+++ example3.scad
@@ -1,1 +1,38 @@
-text( "text", font = "Liberation Sans:style=Bold Italic", size = 25, halign = "center", valign = "center", spacing = 2, direction = "ltr", language = "en", $script = "latin"); 
+color("green"){
+difference(){
+difference(){
+difference(){
+union(){
+rotate([ 45, -45, 45 ]){
+hull(){
+rotate(a = 45, v = [5, 5, 5]){
+translate([ 5, 5, 5 ]){
+translate([ 5, 5, 5 ]){
+color("red"){
+cube([ 5.100000e+00, 5.100000e+00, 5.100000e+00 ]);
+}
+}
+}
+}
+cube([ 10, 10, 10 ]);
+}
+}
+translate([ 5, 0, 20 ]){
+sphere(d=10,  $fn = 35);
+}
+}
+translate([ 2.030504e-01, 8.127286e+00, 1.257848e+01 ]){
+sphere(d=7,  $fn = 35);
+}
+}
+translate([ -7.969322e+00, -2.182905e+00, 1.092332e+00 ]){
+sphere(d=7,  $fn = 35);
+}
+}
+rotate([ 0, 10, 30 ]){
+color("red"){
+cylinder(d1=5, d2=2, h=70, center=true);
+}
+}
+}
+} 
---
+++ scadLinearExtrude.m
@@ -0,0 +1,43 @@
+function square = scadLinearExtrude(height, varargin)
+%scadLinearExtrude -
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% parameters:
+% linear_extrude(height = 5, center = true, convexity = 10, twist = -fanrot, slices = 20, scale = 1.0, $fn = 16)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+center = true;
+position = [];
+param = '';
+color = [];
+while ~isempty(varargin)
+    switch lower(varargin{1})
+        case 'center'
+            param = [param ' center= ' boolean2string(varargin{2}) ','];
+        case 'convexity'
+            param = [param ' convexity= ' num2str(varargin{2}) ','];
+        case 'twist'
+            param = [param ' twist= ' num2str(varargin{2}) ','];
+        case 'slices'
+            param = [param ' slices= ' num2str(varargin{2}) ','];
+        case 'scale'
+            param = [param ' scale= ' num2str(varargin{2}) ','];
+        case 'fn'
+            param = [param ' $fn= ' num2str(varargin{2}) ','];
+        case 'position'
+            position = varargin{2};
+        case 'color'
+            color = varargin{2};
+        otherwise
+            error(['scadSquare: unknown paramiter - ' varargin{1}])
+    end
+    varargin(1:2) = [];
+end
+
+square = ['linear_extrude(size = ' num2str(height) ...
+    param ');' ];
+if ~isempty(position)
+    square =  scadTranslate(position, square);
+end
+if ~isempty(color)
+    square =  scadColor(color, square);
+end
+end
