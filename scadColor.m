2f7321b82226174e83576cd4b9e0a446699ed69f
Alex K <bnax.gm@gmail.com>
2021-09-17 17:28:28 +0300

    scadColor: changed to work with scadStructure

--- scadColor.m
+++ scadColor.m
@@ -1,8 +1,9 @@
-function color_result = scadColor(color, object, varargin)
+function object = scadColor(color, object, varargin)
 %scadColor Summary of this function goes here
 %   Detailed explanation goes here
 color_result = ['color("' char(color) '"){' newline];
-color_result = [color_result char(object) newline];
+color_result = [color_result char(object.structure) newline];
 color_result = ([color_result '}' ]);
+object.structure = color_result;
 end
 
