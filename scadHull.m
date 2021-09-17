222faa0a9aafc30a806c87c7cdd1dab9674d0788
Alex K <bnax.gm@gmail.com>
2021-09-17 17:41:11 +0300

    scadHull: 1. changed to return scadStructure; 2. added parameters color and position.

--- scadHull.m
+++ scadHull.m
@@ -1,7 +1,29 @@
-function hull_result = scadHull(varargin)
-%scadhull Summary of this function goes here
-%   Detailed explanation goes here
+function object = scadHull(array_of_objects, varargin)
+%scadHull  - return the convex hull of objects.
+position = [];
+color = [];
+while ~isempty(varargin)
+    switch lower(varargin{1})
+        case 'position'
+            position = varargin{2};
+        case 'color'
+            color = varargin{2};
+        otherwise
+    end
+    varargin(1:2) = [];
+end
 hull_result = ['hull(){' newline];
-hull_result = [hull_result char(strjoin(varargin, newline)) newline];
+for i = 1:max(size(array_of_objects))
+    object = array_of_objects{i};
+    hull_result = [hull_result char(object.structure) newline];
+end
 hull_result = [hull_result '}' ];
+object = array_of_objects{1};
+object.structure  = hull_result;
+if ~isempty(color)
+    object = scadColor(color, object);
+end
+if ~isempty(position)
+    object =  scadTranslate(position, object);
+end
 end
