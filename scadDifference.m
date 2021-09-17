acb4a7e5414784553062ab0474ea1a8bacad3dd2
Alex K <bnax.gm@gmail.com>
2021-09-17 22:26:24 +0300

    scadDifference: description was updated

--- scadDifference.m
+++ scadDifference.m
@@ -1,6 +1,7 @@
 function difference_result = scadDifference(varargin)
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
+%scadDifference  - Subtracts the 2nd (and all further) child nodes from the
+%first one (logical and not).
+ 
 if isempty(varargin)
     error("scadDifference: There must be at least 2 components")
 else
