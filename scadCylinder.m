eff9343766aef1ee60caf437ba8fa2aa4f728fde
Alex K <bnax.gm@gmail.com>
2021-09-17 22:22:25 +0300

    scadCylinder: description was updated

--- scadCylinder.m
+++ scadCylinder.m
@@ -1,5 +1,5 @@
 function object = scadCylinder(h, d, varargin)
-% scadCylinder - Return string for OpenScad with cylinder discription
+% scadCylinder - Return scadStructure with cylinder discription
 % Parameters
 % h : height of the cylinder or cone
 % r  : radius of cylinder. r1 = r2 = r.
