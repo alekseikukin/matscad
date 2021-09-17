9e9c5f1a0d344ca48ca39b9be2c1962bca6e518b
Alex K <bnax.gm@gmail.com>
2021-09-17 22:37:21 +0300

    scadSquare: added parameter color

--- scadSquare.m
+++ scadSquare.m
@@ -12,29 +12,41 @@ function object = scadSquare(size1, varargin)
 % true, square is centered at (0,0)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
 center = true;
 position = [];
+color = [];
+%
 while ~isempty(varargin)
     switch lower(varargin{1})
         case 'center'
             center = varargin{2};
         case 'position'
             position = varargin{2};
+        case 'color'
+            color = varargin{2};
         otherwise
             error(['scadSquare: unknown paramiter - ' varargin{1}])
     end
     varargin(1:2) = [];
 end
+%
 if max(size(size1)) == 1
     size1 = num2str(size1);
 else
     size1 = ['[' char(strjoin(compose('%d', size1), ',')) ']'];
 end
+%
 square = ['square(size = ' size1 ','...
     ' center =' boolean2string(center) ');' ];
 object = scadStructure();
+%
 object.structure = square;
+%
+if ~isempty(color)
+    object = scadColor(color, object);
+end
 if ~isempty(position)
-    object =  scadTranslate(position, object);
+    object =  scadTranslate(object, circle);
 end
 end
