b85861516e550b6cd57bcddd047349fe13c53eec
Alex K <bnax.gm@gmail.com>
2021-09-17 22:03:35 +0300

    scadTranslate: description was updated

--- scadTranslate.m
+++ scadTranslate.m
@@ -1,6 +1,5 @@
 function object = scadTranslate(points, object, varargin)
-%scadTranslate Summary of this function goes here
-%   Detailed explanation goes here
+%scadTranslate - move object to point [x, y, z] = points
 formatSpec = '[ %d, %d, %d ]';
 points = compose(formatSpec, points);
 translate_result = ['translate(' char(points{:}) '){' newline];
