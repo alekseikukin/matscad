fa373c986a2e5495bdb54bbdfc32a412fcdf9a0a
Alex K <bnax.gm@gmail.com>
2021-09-17 17:29:51 +0300

    scadTranslate: changed to work with scadStructure

--- scadTranslate.m
+++ scadTranslate.m
@@ -1,10 +1,11 @@
-function translate_result = scadTranslate(points, object, varargin)
+function object = scadTranslate(points, object, varargin)
 %scadTranslate Summary of this function goes here
 %   Detailed explanation goes here
-formatSpec = string( [ '[ %d, %d, %d ]' ] );
+formatSpec = '[ %d, %d, %d ]';
 points = compose(formatSpec, points);
 translate_result = ['translate(' char(points{:}) '){' newline];
-translate_result = [translate_result char(object) newline];
+translate_result = [translate_result char(object.structure) newline];
 translate_result = ([translate_result '}' ]);
+object.structure = translate_result;
 end
 
