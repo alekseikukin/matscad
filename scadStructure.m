c54d214a6942ea52b728a646947ad26adcafee97
Alex K <bnax.gm@gmail.com>
2021-09-17 13:56:05 +0300

    class scadStructure was added

--- scadStructure.m
+++ scadStructure.m
@@ -1,22 +1,62 @@
 classdef scadStructure
-    %SCADSTRUCTURE Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    %scadStructure 
     properties
-        structure
+        structure % string with descriprion of structure in openSCAD
     end
-    
+    properties
+        path2SCAD % openSCAD Folder path
+    end
     methods
-        function obj = scadStructure()
-            %SCADSTRUCTURE Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.structure = '';
+        function obj = scadStructure(string, varargin)
+            %scadStructure -            
+            obj.path2SCAD = 'C:\Program Files\OpenSCAD\';
+            if isa(string,scadStructure)
+                obj.structure = string.structure;
+            else
+                obj.structure = '';
+                if ischar(string) || isstring(string)
+                    varargin = {string; varargin{:}};
+                else
+                    disp('scadStructure: first paramiter must be a string or a scadStructure');
+                end
+            end
+            while ~isempty(varargin)
+                switch lower(varargin{1})
+                    case 'path'
+                        obj.path2SCAD  = varargin{2};
+                    otherwise
+                end
+                varargin(1:2) = [];
+            end
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function obj = plus(obj1,obj2)
+            %plus 
+            obj = obj1;
+            obj = scadUnion(obj, obj2);
+        end
+        function obj = minus(obj1,obj2)
+            %minus 
+            obj = obj1;
+            obj = scadDifference(obj, obj2);
+        end
+        function obj = or(obj1,obj2)
+            %or 
+            obj = obj1;
+            obj = scadUnion(obj, obj2);
+        end
+        function obj = and(obj1,obj2)
+            %and 
+            obj = obj1;
+            obj = scadIntersection(obj, obj2);
+        end
+        function result = eq(obj1,obj2)
+            %eq - return resalt of comparation of 2 objects
+            result = obj1.structure == obj2.structure;
+        end
+        function obj = Difference(obj, obj2)
+            %Difference - 
+            obj = obj - obj2;
         end
     end
 end
