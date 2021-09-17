55884d9f733c07b6cd3496dce812318a59378b16
Alex K <bnax.gm@gmail.com>
2021-09-17 22:04:25 +0300

    scadColor: description was updated

--- scadColor.m
+++ scadColor.m
@@ -1,6 +1,5 @@
 function object = scadColor(color, object, varargin)
-%scadColor Summary of this function goes here
-%   Detailed explanation goes here
+%scadColor - set a color of the object
 color_result = ['color("' char(color) '"){' newline];
 color_result = [color_result char(object.structure) newline];
 color_result = ([color_result '}' ]);
