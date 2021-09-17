dc42e561af33133f7c22963eb9fa5caf9ffbe7a2
Alex K <bnax.gm@gmail.com>
2021-09-17 15:06:35 +0300

    added GetParamFromFile - read paramiters from a file

--- GetParamFromFile.m
+++ GetParamFromFile.m
@@ -1,9 +1,19 @@
-function parameter = GetParamFromFile(parameter_name)
+function parameter = GetParamFromFile(parameter_name, varargin)
 %UNTITLED11 Summary of this function goes here
 %   Detailed explanation goes here
-fileID = fopen('ParamitersOpenSCAD', 'r');
-A = fread(fileID);
+file_name = 'ParamitersOpenSCAD';
+while ~isempty(varargin)
+    switch lower(varargin{1})
+        case 'file_name'
+            file_name = varargin{2};
+        otherwise
+    end
+    varargin(1:2) = [];
+end
+fileID = fopen(file_name, 'r');
+A = fscanf(fileID, '%c');
 parameter = extractBetween(A,[parameter_name, ':'], ';');
+parameter = parameter{1};
 if char(parameter(1)) == ' '
     parameter(1) = [];
 end
