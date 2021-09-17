443436b82f02f328f6a126ef86effab0140af054
Alex K <bnax.gm@gmail.com>
2021-09-17 19:16:51 +0300

    scadUnion: 1. changed to work with scadStructure

--- scadUnion.m
+++ scadUnion.m
@@ -2,14 +2,15 @@ function union_result = scadUnion(varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 if isempty(varargin)
-    error("scadUnion: There must be at least 2 components")
+    error("scadunion: There must be at least 2 components")
 else
-    union_result = ['union(){' newline];
+    union_result = varargin{1};
+    union_result.structure = ['union(){' newline];
     while ~isempty(varargin)
-        union_result = [union_result varargin{1} newline];
+        union_result.structure = [char(union_result.structure) char(varargin{1}.structure) newline];
         varargin(1) = [];
     end
-    union_result = [union_result '}' ];
+    union_result.structure = [union_result '}' ];
 end
 end
 
