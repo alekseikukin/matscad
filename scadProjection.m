466936b2d7f14e2cf3e5c1effec5b927a93d4216
Alex K <bnax.gm@gmail.com>
2021-09-17 22:39:10 +0300

    scadProjection: description was updated

--- scadProjection.m
+++ scadProjection.m
@@ -1,6 +1,6 @@
 function projection_result = scadProjection(object, varargin)
-%scadProjection - Using the projection() function, you can create 2d
-%drawings from 3d models, and export them to the dxf format. It works by
+%scadProjection - Using the scadProjection function, you can create 2d
+%drawings from 3d models. It works by
 %projecting a 3D model to the (x,y) plane, with z at 0. If cut=true, only
 %points with z=0 are considered (effectively cutting the object), with
 %cut=false(the default), points above and below the plane are considered as
