6a0bcb82cd4a2ab76933686db97bd0c208a2c0b6
Alex K <bnax.gm@gmail.com>
2021-09-17 22:20:06 +0300

    scadRotate: 1. description was updated; 2. changet parameters and variables; 3. added varargin selection

--- scadRotate.m
+++ scadRotate.m
@@ -1,17 +1,26 @@
-function object = scadRotate(object, varargin)
-%scadrotate Summary of this function goes here
-%   Detailed explanation goes here
-if isvector(varargin{1}) && max(size(varargin{1})) == 3
-    formatSpec = string( [ '[ %d, %d, %d ]' ] );
-    rotation_params = varargin{1};
-    varargin(1) = [];
-elseif isvector(varargin{2}) && max(size(varargin{2})) == 3 && isvector(varargin{1}) && max(size(varargin{1})) == 1
-    formatSpec = string( [ 'a = %d, v = [%d, %d, %d]' ] );
-    rotation_params = [varargin{1}(:), varargin{2}(1,:)];
+function object = scadRotate(angles, object, varargin)
+%scadRotate  - Rotates its child 'a' degrees about the axis of the
+%coordinate system or around an arbitrary axis.
+param = '';
+formatSpec = '[ %d, %d, %d ]';
+while ~isempty(varargin)
+    switch lower(varargin{1})
+        case 'axis'
+            param = [param ', v = ' char(compose(formatSpec, varargin{1}))];
+        otherwise
+            error(['scadRotate: unknown parameter' char(string(varargin{1}))])
+    end
     varargin(1:2) = [];
-else
 end
-rotate_result = ['rotate(' char(compose(formatSpec, rotation_params)) '){' newline];
+
+if max(size(angles)) == 1
+    angles = num2str(angles);
+elseif max(size(angles)) == 3
+    angles =['a = ' char(compose(formatSpec, angles))];
+else
+    error('scadRotate: angle must be vector 1 x 3 [x_angle, y_angle, z_angle] or double')
+end
+rotate_result = ['rotate(' angles param '){' newline];
 rotate_result = [rotate_result char(object.structure) newline];
 rotate_result = ([rotate_result '}' ]);
 object.structure = rotate_result;
