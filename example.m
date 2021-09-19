a398ef092dd447bd455b0d5d9430bf6dbd885bb3
Alex K <bnax.gm@gmail.com>
2021-09-19 22:59:57 +0300

    update example

--- example.m
+++ example.m
@@ -1,4 +1,4 @@
-% generate stl file with cheese inside.
+%%%%%%%%%%%%%%%% generate stl file with cheese inside.%%%%%%%%%%%%%%%%%%%%%
 clear;
 radius = 40;
 thikness = 10;
@@ -8,6 +8,7 @@ test_structure.Square([radius,thikness], 'center', false); % add Square in struc
 test_structure.RotateExtrude(45,'fn', 100); % rotate Square around Oz
 test_structure2 = scadStructure('file_name', 'bubbles', 'autosave', true); % initiate secondary structure
 for i =1:50 % adding in secondary structure 50 bubbles
+    % generation of paramiters of bubbles
     d = rand() * (thikness*0.3) + (thikness*0.3);
     x = radius;
     y = radius;
@@ -20,4 +21,4 @@ for i =1:50 % adding in secondary structure 50 bubbles
     test_structure2.Sphere(d, 'position', [x, y, z], 'fn', 15); 
 end
 test_structure = test_structure - test_structure2;
-test_structure.SaveAs();
+test_structure.SaveAs(); % saving structure as stl, it's possible choose another file format
