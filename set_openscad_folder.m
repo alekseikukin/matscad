d716c263b35c1dd3762e5e07a1879b64dfa30f89
Alex K <bnax.gm@gmail.com>
2021-09-19 23:20:07 +0300

    added example how to set openscad folder 'set_openscad_folder.m'

---
+++ set_openscad_folder.m
@@ -0,0 +1,8 @@
+% change path
+[file_name, path2openscad] = uigetfile('*.exe',...
+    'Select openscad.exe file','C:\Program Files\OpenSCAD\openscad.exe');
+if ~isempty(path2openscad) && string(file_name) == "openscad.exe"
+    SetOpenSCAD('path', path2openscad);
+else
+    disp('folder was not selected')
+end
