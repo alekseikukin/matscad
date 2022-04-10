ea16d88fd174f15dc7e110d754a288ad457f46bf
alekseikukin <bnax.gm@gmail.com>
2022-04-10 09:46:12 +0300

    cmdOpenSCAD was updated to support unix

--- scadColor.m
+++
@@ -1,9 +0,0 @@
-function object = scadColor(color, object, varargin)
-%scadColor - set a color of the object
-%%
-color_result = ['color("' char(color) '"){' newline];
-color_result = [color_result char(object.structure) newline];
-color_result = ([color_result '}' ]);
-object.structure = color_result;
-end
-
